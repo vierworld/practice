963c7d55da17e7b1142a1de467848a375a590674
vierworld <vierworld@gmail.com>
2024-02-11 20:36:11 -0800

    practice3

--- src/main/java/ru/vw/practice/MainApp.java
+++ src/main/java/ru/vw/practice/MainApp.java
@@ -1,9 +1,9 @@
 package ru.vw.practice;
 
-import ru.vw.practice.lesson2.SecondLesson;
+import ru.vw.practice.lesson3.ThirdLesson;
 
 public class MainApp {
   public static void main(String[] args) {
-    SecondLesson.runAllTest();
+    ThirdLesson.runTests();
   }
 }
---
+++ src/main/java/ru/vw/practice/lesson3/ThirdLesson.java
@@ -0,0 +1,88 @@
+package ru.vw.practice.lesson3;
+
+import java.util.List;
+
+public class ThirdLesson {
+  public static Runnable getTestCycled(String name) {
+    return new Runnable() {
+      @Override
+      public String toString() {
+        return name;
+      }
+
+      @Override
+      public void run() {
+        System.out.printf("getTestCycled start: %s\n", this);
+        while (true) {
+          try {
+            Thread.sleep(5000);
+          } catch (InterruptedException e) {
+            throw new RuntimeException("getTestCycled %s was interrupted".formatted(this));
+          }
+          System.out.printf("getTestCycled working: %s\n", this);
+        }
+      }
+    };
+  }
+
+  public static Runnable getTestSimple(String name) {
+    return new Runnable() {
+      @Override
+      public String toString() {
+        return name;
+      }
+
+      @Override
+      public void run() {
+        System.out.printf("getTestSimple start: %s\n", this);
+
+        try {
+          Thread.sleep(1500);
+        } catch (InterruptedException e) {
+          throw new RuntimeException("getTestSimple %s was interrupted".formatted(this));
+        }
+        System.out.printf("getTestSimple working: %s\n", this);
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException e) {
+          throw new RuntimeException("getTestSimple %s was interrupted".formatted(this));
+        }
+
+        System.out.printf("getTestSimple finished: %s\n", this);
+      }
+    };
+  }
+
+  public static void runTests() {
+    CustomTreadPool customTreadPool = new CustomTreadPool(5);
+
+    customTreadPool.execute(List.of(
+            getTestSimple("Thread 1.1"),
+            getTestCycled("Thread 1.2"),
+            getTestSimple("Thread 1.3"),
+            getTestSimple("Thread 1.4")
+    ));
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    customTreadPool.execute(List.of(
+            getTestCycled("Thread 2.1"),
+            getTestCycled("Thread 2.2"),
+            getTestSimple("Thread 2.3"),
+            getTestCycled("Thread 2.4")
+    ));
+    try {
+      Thread.sleep(12000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    customTreadPool.shutdown();
+
+    customTreadPool.execute(getTestCycled("Thread 3.1"));
+  }
+
+}
